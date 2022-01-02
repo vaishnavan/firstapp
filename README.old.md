748ec8048808bd7c0e572f612963effb295c7edf
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2022-01-02 10:16:49 +0530

    app setup

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# my first color picker
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>my first App</h1>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
